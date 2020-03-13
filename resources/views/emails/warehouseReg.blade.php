db96ce3640383f9c76e63561b48e379ecccf5d52
quad-las <quad.lasisi@gmail.com>
2020-03-13 09:06:52 +0100

    updated Warehouse registeration email notification message

--- resources/views/emails/warehouseReg.blade.php
+++ resources/views/emails/warehouseReg.blade.php
@@ -3,6 +3,7 @@ Dear {{ explode(' ', trim($user))[0] }},<br>
 
 Congratulations! <br>
 Your warehouse, <b>{{ $warehouse }}</b>  has been successfully registered on the AgX.africa platform. <br>
+Take a step ahead so merchants around the world can trade your commodities. <br>
 
 
 Mafo,<br>
